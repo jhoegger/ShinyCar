cc98168657c12aababda3684e199b2cc55a2998c
John Hoegger <jhoegger@hotmail.com>
2015-01-25 08:07:54 -0800

    updated with additional plots

--- server.R
+++ server.R
@@ -1,29 +1,29 @@
 library(shiny)
 library(UsingR)
-data(galton)
 library(datasets)
 data(mtcars)
 
 ###Make the data tidy
-#mtcars$cyl  <-as.factor(mtcars$cyl)
 mtcars$vs   <-as.factor(mtcars$vs)
 mtcars$am[mtcars$am==0]<-"Auto"
 mtcars$am[mtcars$am==1]<-"Manual"
 mtcars$am   <-as.factor(mtcars$am)
-#mtcars$gear <-as.factor(mtcars$gear)
 mtcars$carb <-as.factor(mtcars$carb)
 
 shinyServer(
     function(input, output){
+        #output selected values
         output$onumCly <- renderPrint({input$idCyl})
         output$onumGears <- renderPrint({input$idGears})
         output$ocarCount <- renderPrint({
         numCyl <- {input$idCyl}
         numGears <- {input$idGears}
+        #show the total number of items selected
         submtcars <- mtcars[mtcars$cyl<=numCyl & mtcars$gear<=numGears,]
         nrow(submtcars)
         })
-
+        
+        #generate the summary of the model
         output$fitSum <- renderPrint({
         numCyl <- {input$idCyl}
         numGears <- {input$idGears}
@@ -32,6 +32,7 @@ shinyServer(
         summary(bestFit)
         })
 
+        #return a boxplot of MPG vs. transmission type            
         output$newBoxplot <- renderPlot({
             numCyl <- {input$idCyl}
             numGears <- {input$idGears}
@@ -40,6 +41,7 @@ shinyServer(
                     ylab="mpg",xlab="transmission",main="MPG vs Transmission")
         })
         
+        #return the pairs plot of all variables 
         output$newPairs <- renderPlot({
             numCyl <- {input$idCyl}
             numGears <- {input$idGears}
@@ -48,6 +50,15 @@ shinyServer(
             main="Car Analysis", col=3)
         })
 
-        
+        #return the residual plot and diagnostics
+        output$newBestFit <- renderPlot({
+            numCyl <- {input$idCyl}
+            numGears <- {input$idGears}
+            submtcars <- mtcars[mtcars$cyl<=numCyl & mtcars$gear<=numGears,]
+            fit <- lm(mpg ~ am, submtcars)
+            bestFit <- step(fit, direction = "backward")
+            par(mfrow=c(2, 2))
+            plot(bestFit)
+        })
     }
 )
--- ui.R
+++ ui.R
@@ -2,6 +2,9 @@ shinyUI(pageWithSidebar(
     headerPanel("Motor Trends Car Analysis for 1973-74"),
     sidebarPanel(
         h3('Cylinder & Gear Selection'),
+        
+        p('Use the slides to select the category of cars included in the analysis'),
+        
         #Add a slide bar to select the number of cylinders
         sliderInput('idCyl', 'Select the minimum number of cylinders', value=6, min=4, max=8, step=2),
         h4('Number of Cylinders'),
@@ -16,11 +19,13 @@ shinyUI(pageWithSidebar(
         
         ),
     mainPanel(
-        p('Using the Motor Trends Car Analysis for 1973-74 data set I am interested in'),
-        p('exploring the relationship between a set of variables and miles per gallon (MPG).'),
+        p('Using the Motor Trends Car Analysis for 1973-74 data set'),
+        p('Exploring the relationship between a set of variables and miles per gallon (MPG).'),
+        p('The model includes all available variables to show the impact and it uses the step function.'),
         h4('Number of cars in the model'),
         verbatimTextOutput("ocarCount"), #get the count of the number of cars in the model
         plotOutput('newBoxplot'),   #newBoxplot is defined on the server
-        plotOutput('newPairs')      #newPairs is defined on the server
+        plotOutput('newPairs'),      #newPairs is defined on the server
+        plotOutput('newBestFit')      #newPairs is defined on the server
         )
 ))
